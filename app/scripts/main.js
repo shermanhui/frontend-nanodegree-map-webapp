84466a0cdcd92d765dcc10c40a2edbac7553b747
shui91 <sherman.sy.hui@gmail.com>
2015-08-16 01:59:19 -0700

    in progress: implementing viewModel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,31 @@
 var map;
 
-function initMap() {
-	var vancouver = {lat: 49.2827, lng: -123.1207};
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: vancouver,
-		zoom: 14
-	});
+// function initMap() {
+// 	var vancouver = {lat: 49.2827, lng: -123.1207};
+// 	map = new google.maps.Map(document.getElementById('map'), {
+// 		center: vancouver,
+// 		zoom: 14,
+// 		disableDefaultUI: true
+// 	});
+// }
+
+function viewModel() {
+
+	function initMap(){
+		var options, mapContainer, vancouver
+
+		options = {
+			vancouver: {lat: 49.2827, lng: -123.1207},
+			center: vancouver,
+			zoom: 14,
+			disableDefaultUI: true
+		};
+
+		mapContainer = document.getElementById('map');
+
+		return new google.maps.Map(options, mapContainer);
+	};
 }
+
+
+ko.applyBindings(new viewModel());
