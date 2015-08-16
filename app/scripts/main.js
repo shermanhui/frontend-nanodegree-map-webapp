2d1d0c90d315d5b2b1618f9141e7447e23f6f88b
shui91 <sherman.sy.hui@gmail.com>
2015-08-16 13:29:24 -0700

    feat: cleaned things up, starting to look better

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 var map;
 
+// original code from Google Maps API
 // function initMap() {
 // 	var vancouver = {lat: 49.2827, lng: -123.1207};
 // 	map = new google.maps.Map(document.getElementById('map'), {
@@ -12,19 +13,22 @@ var map;
 var viewModel = function() {
 	var self = this;
 
-	var options, mapContainer;
+	var options, mapContainer, myLocation;
+	
+	myLocation = new google.maps.LatLng(49.2827, -123.1207);
+	// vancouver = {lat: 49.2827, lng: -123.1207};
+	self.initMap = function(){
+		mapContainer = document.getElementById('map');
 
-	var myLocation = new google.maps.LatLng(49.2827, -123.1207);
-		// vancouver = {lat: 49.2827, lng: -123.1207};
-	mapContainer = document.getElementById('map');
-	options = {
-		center: myLocation,
-		zoom: 14,
-		disableDefaultUI: true
+		options = {
+			center: myLocation,
+			zoom: 14,
+			disableDefaultUI: true
+		};
+
+		map = new google.maps.Map(mapContainer, options);
 	};
-	map = new google.maps.Map(mapContainer, options);
-
-	return map;
+	self.initMap();
 };
 
 ko.applyBindings(new viewModel());
