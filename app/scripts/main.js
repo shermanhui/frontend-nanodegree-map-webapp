bcae8b8be7a4fa2e860041fcda5cb0b81433328c
shui91 <sherman.sy.hui@gmail.com>
2015-08-16 20:37:36 -0700

    chore: going back to basics, just building regular markers on googleMaps to see how it's done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,52 @@
 var map;
 
 // original code from Google Maps API
-// function initMap() {
-// 	var vancouver = {lat: 49.2827, lng: -123.1207};
-// 	map = new google.maps.Map(document.getElementById('map'), {
-// 		center: vancouver,
-// 		zoom: 14,
-// 		disableDefaultUI: true
-// 	});
-// }
+function initMap() {
+	var vancouver = {lat: 49.2827, lng: -123.1207};
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: vancouver,
+		zoom: 14,
+		disableDefaultUI: true
+	});
+}
 
-var viewModel = function() {
-	var self = this;
-	var options, mapContainer, myLocation;
+var myLatlng = {lat: 49.2844, lng: -123.1089};
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    title:"Hello World!"
+});
+initMap();
+// To add the marker to the map, call setMap();
+marker.setMap(map);
 
-	// vancouver = {lat: 49.2827, lng: -123.1207};
-	self.initMap = function(){
-		myLocation = new google.maps.LatLng(49.2827, -123.1207);
-		mapContainer = document.getElementById('map');
 
-		options = {
-			center: myLocation,
-			zoom: 14,
-			disableDefaultUI: true
-		};
+// var exampleData = [
+// 	{
+// 		'lat': 49.2844, 
+// 		'lng': -123.1089,
+// 		'name': "steam clock"
+// 	}
+// ];
 
-		map = new google.maps.Map(mapContainer, options);
-	};
-	self.initMap();
-};
+// var viewModel = function() {
+// 	var self = this;
 
-ko.applyBindings(new viewModel());
+// 	var options, mapContainer, myLocation;
+
+// 	// vancouver = {lat: 49.2827, lng: -123.1207};
+// 	initMap = function(){
+// 		myLocation = new google.maps.LatLng(49.2827, -123.1207);
+// 		mapContainer = document.getElementById('map');
+
+// 		options = {
+// 			center: myLocation,
+// 			zoom: 14,
+// 			disableDefaultUI: true
+// 		};
+
+// 		map = new google.maps.Map(mapContainer, options);
+// 	};
+// 	initMap();
+// };
+
+// ko.applyBindings(new viewModel());
