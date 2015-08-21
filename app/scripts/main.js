de6e7e37b8eee3dd0cec24c3af6bd393b6e25a3c
shui91 <sherman.sy.hui@gmail.com>
2015-08-21 15:05:22 -0700

    feat: factored out contentString details, still building searchbar functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,7 +19,7 @@ var locations = [
 		'lng': -123.103834,
 		'desc': 'How do magnets work..'
 	}
-]
+];
 var markerArray = [];
 // original code from Google Maps API
 function initMap() {
@@ -30,48 +30,54 @@ function initMap() {
 		zoom: 14,
 		disableDefaultUI: false
 	});
-
 	for (i = 0; i < locations.length; i++){
 		marker = new google.maps.Marker({
 		position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
 		map: map,
 		animation: google.maps.Animation.DROP
 	});
-
+		markerArray.push(marker);
 		google.maps.event.addListener(marker, 'click', (function(marker, i){
 			return function(){
-				var contentString =
-					'<div id="content">'+
-					'<div id="siteNotice">'+
-					'</div>'+
-					'<h1 id="firstHeading" class="firstHeading">'+ locations[i].name +'</h1>'+
-					'<div id="bodyContent">'+
-					'<p><b>Interesting location description</b></p>'+
-					'<p>'+ locations[i].desc + '<p>' +
-					'</div>'+
-					'</div>';
+				var contentString = getContentString(locations[i]);
 				infoWindow.setContent(contentString);
 				infoWindow.open(map, marker);
-			}
+			};
 		})(marker, i));
-	};
+	}
 }
+
+var getContentString = function(location) {
+    var contentString =
+    	'<div id="content">'+
+		'<div id="siteNotice">'+
+		'</div>'+
+		'<h1 id="firstHeading" class="firstHeading">'+ location.name +'</h1>'+
+		'<div id="bodyContent">'+
+		'<p><b>Interesting location description</b></p>'+
+		'<p>'+ location.desc + '<p>' +
+		'</div>'+
+		'</div>';
+    return contentString;
+};
+
 
 
 
 function viewModel() {
-	markers = ko.observableArray[locations],
-	query = ko.observable('')
+	var self = this;
+	this.markers = ko.observableArray(markerArray);
+	this.query = ko.observable('');
 
-	search = function(value) {
-		viewModel.markers.removeAll();
-		for(var x in markers) {
-			if(markers[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-				viewModel.markers.push(markers[x]);
+	this.search = function(value) {
+		self.markers.removeAll();
+		for(var x in self.markers) {
+			if(self.markers[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+				self.markers.push(self.markers[x]);
 			}
 		}
 	};
-	query.subscribe(viewModel.search);
-};
-console.log(markerArray);
+	this.query.subscribe(self.search);
+}
+
 ko.applyBindings(new viewModel());
