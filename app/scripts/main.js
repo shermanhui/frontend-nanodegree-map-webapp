4088f729d8bc49ff889507dfccf5f4e23fe651b9
shui91 <sherman.sy.hui@gmail.com>
2015-08-25 11:28:38 -0700

    feat: now making markers from FourSquare data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 var map, infoWindow, i;
 var CLIENT_ID = 'Q0A4REVEI2V22KG4IS14LYKMMSRQTVSC2R54Y3DQSMN1ZRHZ';
 var CLIENT_SECRET = 'NPWADVEQHB54FWUKETIZQJB5M2CRTPGRTSRICLZEQDYMI2JI';
-var fourSquare_URL = 'https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20150825';
+var fourSquare_URL = 'https://api.foursquare.com/v2/venues/search?near=Vancouver,BC&categoryId=4bf58dd8d48988d116941735&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20150825';
+var locName, locAddress, locContact, locLat, locLng;
 
 var myLatlng = {lat: 49.2844, lng: -123.1089};
 var locations = [
@@ -34,44 +35,85 @@ var markers = [];
 
 // original code from Google Maps API
 function initMap() {
-	//var bounds = new google.maps.LatLngBounds();
+	var bounds = new google.maps.LatLngBounds();
 	infoWindow = new google.maps.InfoWindow();
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: myLatlng,
 		zoom: 14,
 		disableDefaultUI: false
 	});
-	for (i = 0; i < locations.length; i++){
+	// for (i = 0; i < locations.length; i++){
+	// 	marker = new google.maps.Marker({
+	// 	position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+	// 	map: map,
+	// 	animation: google.maps.Animation.DROP
+	// });
+	// 	markers.push(marker);
+	// 	google.maps.event.addListener(marker, 'click', (function(marker, i){
+	// 		return function(){
+	// 			var contentString = getContentString(locations[i]);
+	// 			infoWindow.setContent(contentString);
+	// 			infoWindow.open(map, marker);
+	// 		};
+	// 	})(marker, i));
+	// }
+}
+
+function makeMarker(data){
+	var venueData = data.response.venues
+	var dataLen = venueData.length
+	for (var i = 0; i < dataLen; i++){
+		locName = data.response.venues[i].name;
+		locAddress = data.response.venues[i].location.address;
+		locContact = data.response.venues[i].contact.formattedPhone;
+		locLat = data.response.venues[i].location.lat;
+		locLng = data.response.venues[i].location.lng;
+
 		marker = new google.maps.Marker({
-		position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
-		map: map,
-		animation: google.maps.Animation.DROP
-	});
+			position: new google.maps.LatLng(locLat, locLng),
+			map: map,
+			animation: google.maps.Animation.DROP
+		});
 		markers.push(marker);
 		google.maps.event.addListener(marker, 'click', (function(marker, i){
 			return function(){
-				var contentString = getContentString(locations[i]);
+				var contentString = getContentString(venueData[i]);
 				infoWindow.setContent(contentString);
 				infoWindow.open(map, marker);
 			};
 		})(marker, i));
+		//console.log(data.response.venues[i]);
 	}
 }
 
-var getContentString = function(location) {
+//get FourSquare data
+function getData(){
+	$.ajax(fourSquare_URL, {
+		dataType: 'json',
+		type: 'GET',
+		success: function(data){
+			makeMarker(data);
+			// console.log(data.response.venues);
+			// console.log(data.response.venues[5].name + ', ' + data.response.venues[5].location.address + ', ' + data.response.venues[0].contact.formattedPhone);
+			// console.log(data.response.venues[5].location.lat, data.response.venues[5].location.lng);
+		}
+	});
+};
+
+// content string for infoWindow
+var getContentString = function(venueData) {
     var contentString =
     	'<div id="content">'+
 		'<div id="siteNotice">'+
 		'</div>'+
-		'<h1 id="firstHeading" class="firstHeading">'+ location.name +'</h1>'+
+		'<h1 id="firstHeading" class="firstHeading">'+ venueData.name +'</h1>'+
 		'<div id="bodyContent">'+
-		'<p><b>Interesting location description</b></p>'+
-		'<p>'+ location.desc + '<p>' +
+		'<p><b>Address and Contact</b></p>'+
+		'<p>'+ venueData.location.address + ' '+ venueData.contact.formattedPhone + '</p>' +
 		'</div>'+
 		'</div>';
     return contentString;
 };
-
 
 function viewModel() {
 	var self = this;
@@ -92,17 +134,24 @@ function viewModel() {
 			}
 		}
 	};
-	this.getData = function() {
-		$.ajax(fourSquare_URL, {
-			dataType: 'json',
-			//type: 'GET',
-			success: function(response){
-				console.log(response);
-			}
-		});
-	};
-	this.getData();
+	// this.filteredLocations = ko.computed(function(){
+	// 	var search = self.query().toLowerCase();
+	// 	return ko.utils.arrayFilter(self.filteredLocations, function(locations){
+	// 		return locations.name.toLowerCase().indexOf(search) >= 0;
+	// 	});
+	// }, viewModel);
+
+	// this.getData = function() {
+	// 	$.ajax(fourSquare_URL, {
+	// 		dataType: 'json',
+	// 		type: 'GET',
+	// 		success: function(response){
+	// 			console.log(response);
+	// 		}
+	// 	});
+	// };
+	// this.getData();
 	this.query.subscribe(this.search);
 }
-
+getData();
 ko.applyBindings(new viewModel());
